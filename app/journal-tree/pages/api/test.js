570295045e96af866adb168d25c90698930d32b0
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-06-21 00:01:18 +0100

    Add python api example

--- app/journal-tree/pages/api/test.js
+++ app/journal-tree/pages/api/test.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ result: 'OK' })
+    res.status(200).json({ result: 'JS API OK' })
 }
--- app/journal-tree/pages/apiExample.js
+++ app/journal-tree/pages/apiExample.js
@@ -1,28 +1,47 @@
 import {useState, useEffect} from 'react'
-
+import Link from "next/link";
 
 export default function ApiExample() {
 
-    const [data, setData] = useState(null)
+    const [jsData, setJsData] = useState(null)
+    const [pythonData, setPythonData] = useState(null)
     const [isLoading, setLoading] = useState(false)
 
     useEffect(() => {
-        const getApiTest = async () => {
+        const getJSApiTest = async () => {
             const res = await fetch('/api/test')
             const json = await res.json()
-            setData(json)
+            setJsData(json)
             setLoading(false)
         }
-        getApiTest()
+        getJSApiTest()
+
+        const getPythonApiTest = async () => {
+            setLoading(true)
+            const res = await fetch('/api/python')
+            const json = await res.json()
+            setPythonData(json)
+            setLoading(false)
+        }
+        getPythonApiTest()
 
     }, [])
 
-
+    let result = []
     if (isLoading) {
-        return <p>Loading...</p>
-    } else if (!data) {
-        return <p>Waiting for API result</p>
+        result = [<p key={"loading"}>Loading...</p>]
     } else {
-        return <div>Result of API call:{data.result}</div>
+        result = []
+        if (!jsData) {
+            result.push(<p key={"js_wait"}>Waiting for JS API result</p>)
+        } else {
+            result.push(<div key={"js"}>Result of JS API call:{jsData.result}</div>)
+        }
+        if (!pythonData) {
+            result.push(<p key={"python_wait"}>Waiting for Python API result</p>)
+        } else {
+            result.push(<div key={"python"}>Result of Python API call:{pythonData.message}</div>)
+        }
     }
+    return result
 }
