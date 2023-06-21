3de75773987f911b165c5f3621f72f11789163fc
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-06-20 23:51:59 -0500

    Update daily_entry_to_json.py

--- scripts/daily_entry_to_json.py
+++ scripts/daily_entry_to_json.py
@@ -15,6 +15,7 @@ openai_api_key = 'YOUR_API_KEY'
 # Temperature is set to 0 for deterministic output.
 llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
 
+# Prompt to Summarize the data.
 # The area after the TEXT: should come from the app when the user enters the data. I haven't taken it as an input for demonstration purpose.
 prompt = """
 Please provide a summary of the individuals day from the answers to the questions in less than 100 words ?
@@ -43,8 +44,49 @@ TEXT:
 I was trying to minimize the tokens that is the reason why the prompt is to summarize the data in less than 100 words.
 """
 num_tokens = llm.get_num_tokens(prompt)
-print (f"Our prompt has {num_tokens} tokens")
+# print (f"Our prompt has {num_tokens} tokens")
 
 # Getting the output.
 entry = llm(prompt)
-print(entry)
+# print(entry)
+
+# Prompt for finding the mood of the user of the day from the summary of his/ her journal data.
+prompt = f"""
+In one word, describe the mood of the person of the day.
+
+TEXT:
+{entry}
+"""
+
+num_tokens = llm.get_num_tokens(prompt)
+print (f"Our prompt has {num_tokens} tokens")
+
+mood = llm(prompt)
+print(mood)
+
+filename = '/content/student_journal.json'
+listObj = []
+# Check if file exists
+
+if path.isfile(filename) is False:
+  raise Exception("File not found")
+
+# Read JSON file
+with open(filename) as fp:
+  listObj = json.load(fp)
+
+# Verify existing list
+print(listObj)
+print(type(listObj))
+listObj['entries'].append({
+  "mood": mood.strip(),
+  "entry": entry.strip(),
+})
+
+# Verify updated list
+print(listObj)
+with open(filename, 'w') as json_file:
+    json.dump(listObj, json_file,
+                        indent=4,
+                        separators=(',',': '))
+print('Successfully appended to the JSON file')
