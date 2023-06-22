5f04322218270f891d90298fd7cb4e057eab1d9e
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-22 12:09:45 -0400

    Css Sheet Update

--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import React from 'react';
+import 'Journal-Tree/Journal-Tree/app/journal-tree/src/app/_stylesheet/style.css';  // import the CSS file
+
+
+
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <p1>Welcome Back, Sam</p1>
+
       </div>
     </main>
   )
