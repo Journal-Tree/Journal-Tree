80f2603e6d33b5742614dca29cee371b2ea9e795
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-21 20:34:54 -0400

    LayoutChanges

--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Change this Layout
           </p>
         </a>
       </div>
