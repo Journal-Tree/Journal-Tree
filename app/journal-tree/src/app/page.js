c60ea85d10cc655ec54a5c52ea1a79aaccc291bc
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-22 23:33:41 -0400

    Component Updates

--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -1,17 +1,13 @@
 import Image from 'next/image'
-
+import welcomeBlock from  'Journal-Tree/Journal-Tree/app/journal-tree/src/app/components/welcomeBlock.js'
 import './_stylesheet/style.css';  // import the CSS file
-
-
-
-
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p1>Welcome Back, Sam</p1>
-
+        <h1>Welcome Bck, Sam</h1>
+        <welcomeBlock/>
       </div>
     </main>
   )
