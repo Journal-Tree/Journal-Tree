790f4255e235cb1dedcec6267a78591a007f925e
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-23 13:25:41 -0400

    chart.js component

---
+++ app/journal-tree/src/app/components/ResultsScreen/Chart.js
@@ -0,0 +1,45 @@
+
+const styles = {
+  width: "100%",
+  height: "100%",
+  background: "white",
+  boxShadow: "0px 5px 14px rgba(8, 15, 52, 0.04)",
+  borderRadius: "30px",
+  border: "0.50px solid #EFF0F6",
+  padding: '20px' // padding added for better look and feel
+};
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const ChartComponent = () => (
+  <div style={styles}>
+    <Bar data={data} />
+  </div>
+);
+
+export default ChartComponent;
