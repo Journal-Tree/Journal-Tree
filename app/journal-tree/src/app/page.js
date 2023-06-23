c30c4140a696164cc6fcfc8382d30effbbb0cead
Peter Gelderbloem <petersgelderbloem@gmail.com>
2023-06-23 10:04:47 +0100

    fixed path

--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
-import WelcomeBlock from  './components/welcomeBlock.js'
-import './_stylesheet/style.css';  // import the CSS file
+
+import './_stylesheet/style.css';
+import WelcomeBlock from "./components/WelcomeBlock";
 
 export default function Home() {
   return (
