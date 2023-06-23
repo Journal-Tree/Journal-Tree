a214cb34d5e06af7878c53c997cc9ea94ba911a3
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-23 14:56:13 -0400

    Page.JS Update

--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -2,14 +2,20 @@ import './_stylesheet/style.css';
 import WelcomeBlock from './components/HomeScreen/WelcomeBlock';
 import UserInputBox from './components/ResuableComponents/UserInputBox';
 import UserSubmitButton from './components/UserSubmitButton';
+import CreateYourOwnTag from './components/HomeScreen/MoodTags/CreateYourOwnTag'
+import InspiredTag from './components/HomeScreen/MoodTags/InspiredTag'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Welcome Back, Sam</h1>
+        <h1>Welcome Back, Testing</h1>
+        <WelcomeBlock/>
         <UserInputBox/>
         <UserSubmitButton/>
+        <CreateYourOwnTag/>
+        <InspiredTag/>
       </div>
     </main>
   )
