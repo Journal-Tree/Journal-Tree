2d1b2084643213391c34a9f0ae8c06da6fdafc0f
Shantalia <71754706+Stp155906@users.noreply.github.com>
2023-06-23 08:50:14 -0400

    Testing to see if there is any style updates

---
+++ app/journal-tree/src/app/components/HomeScreen/UserInputBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Stylesheet
+import './UserInputBox.css';
+
+class UserInputBox extends React.Component {
+    render() {
+        return (
+            <textarea className="user-input-box"></textarea>
+        );
+    }
+}
+
+export default UserInputBox;
--- app/journal-tree/src/app/page.js
+++ app/journal-tree/src/app/page.js
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-
+import UserInputBox from 'app/journal-tree/src/app/components/HomeScreen/UserInputBox.js'
 import './_stylesheet/style.css';
 import WelcomeBlock from "./components/WelcomeBlock";
 
@@ -7,8 +7,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Welcome Bck, Sam</h1>
+        <h1>Welcome Back, Sam</h1>
         <WelcomeBlock/>
+        <UserInputBox/>
+        <UserInputBox/>
       </div>
     </main>
   )
